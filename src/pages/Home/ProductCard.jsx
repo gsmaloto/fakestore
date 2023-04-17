5efdabb1797d87de5eb83af936efda2c3b56b1b5
gsmaloto <genermaloto@gmail.com>
2023-04-17 19:51:51 +0800

    improve styles

--- src/pages/Home/ProductCard.jsx
+++ src/pages/Home/ProductCard.jsx
@@ -5,20 +5,20 @@ import { Link } from "react-router-dom";
 const ProductCard = ({ product, isLoading }) => {
   return (
     <Link to={`/product/${product.id}`}>
-      <div className="w-[140px] h-[200px] md:w-[200px] md:h-[250px] cursor-pointer bg-gray-100 hover:shadow-xl group relative">
+      <div className="xl:w-[200px]  h-[200px] md:h-[250px] cursor-pointer bg-gray-100 hover:shadow-xl group relative">
         <div key={product.id}>
           <div className="bg-white w-full h-[90px] md:h-[120px] overflow-hidden">
             <img
               src={product.image}
               alt={product.title}
-              className="object-contain object-center mx-auto h-full group-hover:scale-125 "
+              className="object-contain object-center h-full mx-auto group-hover:scale-125 "
             />
           </div>
 
-          <h4 className="text-sm font-semibold line-clamp-2 px-2 pt-2">
+          <h4 className="px-2 pt-2 text-sm font-semibold line-clamp-2">
             {product.title || <Skeleton />}
           </h4>
-          <div className="flex flex-col md:flex-row justify-between px-4 py-2">
+          <div className="flex flex-col justify-between px-4 py-2 md:flex-row">
             <p>⭐{product.rating.rate}/5</p>
             <p className="font-bold">
               {product.rating.count} <span className="font-medium">Sold</span>
